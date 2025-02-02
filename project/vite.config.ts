e0f506f14cc718a8ee81ab896ab504d44e1e60c6
SKB <shubhambhilare4899223@gmail.com>
2025-02-02 14:17:40 +0530

    changed frontend package

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -3,6 +3,13 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build:{
+    rollupOptions:{
+      output:{
+        manualChunks:undefined
+      }
+    }
+  },
   base:"/" ,
   plugins: [react()],
   optimizeDeps: {
