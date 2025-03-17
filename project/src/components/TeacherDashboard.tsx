09c0220f1749ff06e59bba444ae56d720fa0345c
SKB-3611 <binod.58902@gmail.com>
2025-03-17 05:59:22 +0000

    teahers fix

--- project/src/components/TeacherDashboard.tsx
+++ project/src/components/TeacherDashboard.tsx
@@ -55,7 +55,7 @@ export default function TeacherDashboard() {
     } finally {
       setIsLoading(false)
     }
-  }, [user, days])
+  }, [])
 
   return (
     <div className="min-h-screen bg-gray-50">
